ea176d5e53d8b256981a8d02030930c8473f5730
Felpis <3369363412@qq.com>
2023-06-13 10:51:19 +0800

    new file:   gui/__init__.py 	new file:   gui/mainwin.py 	modified:   main.py 	new file:   tools/game.py 	new file:   tools/web.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import mcpi
+import mcpi.minecraft as minecraft
 
+# 设置服务器地址和端口
+server_address = "localhost"  # 服务器 IP 或 "localhost"
+server_port = 4711            # Raspberry Juice 默认端口
 
+# 连接到 Minecraft 服务器
+mc = minecraft.Minecraft.create(server_address, server_port)
+
+# 发送消息到 Minecraft 中
+mc.postToChat("Hello, Minecraft!")
