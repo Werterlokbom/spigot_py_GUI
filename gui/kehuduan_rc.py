09798247cda74744cbcbef6e3d2880e804a5570d
Felpis <3369363412@qq.com>
2023-06-15 19:42:29 +0800

    Changes to be committed: 	modified:   gui/kehuduan_rc.py 	deleted:    gui/kehuduan_ui.py 	modified:   gui/mainwin.py 	modified:   main.py

--- gui/kehuduan_rc.py
+++ gui/kehuduan_rc.py
@@ -5,6 +5,21 @@
 # Created by: The Resource Compiler for PyQt5 (Qt v5.15.2)
 #
 # WARNING! All changes made in this file will be lost!
+#Copyright (C) 2023  Flepis
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    e-mail: 3369363412@qq.com
 
 from PyQt5 import QtCore
 
